24b417f867f7a08b8576b62e933dbf0a0a6c5d6c
JoYoungHyun <younghyun550@gmail.com>
2025-03-07 17:00:14 +0900

    ReactQueryDevtools 추가 및 Detail 페이지의 쿼리 키를 'detail'로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./shared/Router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <Router />
     </QueryClientProvider>
   );
